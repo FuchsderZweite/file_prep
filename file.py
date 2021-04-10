56e7fef7f3b13c6f7063f64dbaf8c3e2511751eb
sergej <email.sergej@web.de>
2021-04-10 18:41:59 +0200

    working version

---
+++ file.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+base_dir = r'/home/sergej/Desktop/abc'
+
+
+def separate_suffix(filename):
+    split_list = filename.split('.')
+    if len(split_list) > 1:
+        suffix = split_list[-1]
+        return filename[:-(len(suffix)+1)], suffix
+    else:
+        return filename, ''
+
+
+def rename_file(dir=None, rep=None, repw=None):
+    if dir and rep and repw is not None:
+        start_time = time.time()
+
+        n = 0
+        for subdir, dirs, files in os.walk(dir):
+            for file in files:
+                if rep in file:
+                    n += 1
+                    new_file = file.replace(rep, repw, 1)
+                    os.rename(os.path.join(subdir, file), os.path.join(subdir, new_file))
+                    print(new_file)
+        if 0 < n:
+            print(f'Done. {n} files were renamed.')
+        else:
+            print(f'No {rep} were found. Are you sure you spelled {rep} correct? Also check base directory.')
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+
+def main():
+    rename_file(base_dir, rep='TEST', repw='GFK')
+
+
+if __name__ == '__main__':
+    main()
+
+
