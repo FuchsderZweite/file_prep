a8b67b33c7353652b955ec6d019122307334dbb2
Sergej Grischagin <email.sergej@web.de>
2021-04-12 12:53:55 +0200

    second commit

--- file.py
+++ file.py
@@ -8,6 +8,7 @@ def separate_suffix(filename):
     else:
         return filename, ''
 
+
 def rename_file(dir, rep, repw):
     if isinstance(dir, str) and isinstance(rep, str) and isinstance(repw, str):
         n = 0
@@ -17,7 +18,6 @@ def rename_file(dir, rep, repw):
                     n += 1
                     new_file = file.replace(rep, repw, 1)
                     os.rename(os.path.join(subdir, file), os.path.join(subdir, new_file))
-                    print(new_file)
         if 0 < n:
             print(f'Done. {n} files were renamed.')
         else:
@@ -29,11 +29,25 @@ def rename_file(dir, rep, repw):
         print(f'=> passed value for {rep} is a {type(rep)}')
         print(f'=> passed value for {repw} is a {type(repw)}')
 
+
+def move_files(dir, create_folder, dir_name):
+    if create_folder:
+        for subdir, dirs, files in os.walk(dir):
+            new_folder = subdir + dirs + '\images'
+            if not os.path.exists(new_folder):
+                os.makedirs(dir_name)
+                os.rename(subdir + dirs + files, subdir + dirs)
+    #else:
+
+
+
 def main():
-    base_dir = r' '
-    replace = ''
-    replace_with = ' '
+    base_dir = r'\\132.187.193.8\junk\sgrischagin\Dritte_Messung'
+    replace = '_0_'
+    replace_with = '_refs_'
     rename_file(dir=base_dir, rep=replace, repw=replace_with)
+
+    #move_files(dir=base_dir, create_folder=True, dir_name='images')
 
 if __name__ == '__main__':
     main()
